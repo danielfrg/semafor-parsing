473f2584735466297f726e7f565ec6ec22d12e43
Daniel Rodriguez <drodriguez@oculus360.us>
2013-11-17 20:51:07 -0600

    Fixed celery cmd

--- semafor/minion/start_worker.sh
+++ semafor/minion/start_worker.sh
@@ -11,4 +11,4 @@ fi
 
 export PYTHONPATH=/home/$(echo $USER)/semafor/app:$PYTHONPATH
 
-/home/$(echo $USER)/venv/bin/celeryd worker --app=semafor.minion.worker.celery -l info --broker=$1 -Q semafor.minion
+/home/$(echo $USER)/venv/bin/celery worker --app=semafor.minion.worker.celery -l info -D -Q semafor.minion -c 1 --broker=$1

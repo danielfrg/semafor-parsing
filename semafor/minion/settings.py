c19a715395d584adf69075bbfe5c686610c4800a
Daniel Rodriguez <drodriguez@oculus360.us>
2013-11-19 22:13:03 -0600

    local_settings.py option

--- semafor/minion/settings.py
+++ semafor/minion/settings.py
@@ -9,7 +9,11 @@ URLS_FILE = 'urls.input'
 DATA_DIR = '/home/ubuntu/semafor/data'
 SEMAFOR_DIR = '/home/ubuntu/semafor/semafor'
 
-import os
+try:
+    from local_settings import *
+except:
+    pass
 
+import os
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)

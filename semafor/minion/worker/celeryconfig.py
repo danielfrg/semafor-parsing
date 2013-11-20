c0d555dd6b8a36387828a688810efafa0c25aaf5
Daniel Rodriguez <drodriguez@oculus360.us>
2013-11-19 22:32:01 -0600

    CELERYD_PREFETCH_MULTIPLIER = 1 because long running task

--- semafor/minion/worker/celeryconfig.py
+++ semafor/minion/worker/celeryconfig.py
@@ -4,6 +4,8 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 
+CELERYD_PREFETCH_MULTIPLIER = 1
+
 CELERY_IMPORTS = ('semafor.minion.worker.tasks', )
 
 CELERY_ROUTES = {'semafor.minion.worker.tasks.run_semafor': {'queue': 'semafor.minion'}}
